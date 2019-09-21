b2dcb5d0a7b421a4f6f0f1104cfa4f6b6c7bbd21
Matt Dalton <mdalton142@gmail.com>
2019-09-21 15:33:01 -0400

    Adding django-heroku. Now in settings as well

--- recap/settings.py
+++ recap/settings.py
@@ -148,3 +148,7 @@ STATIC_URL = '/static/'
 # Deployment setting.
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Activate Django-Heroku.
+import django_heroku
+django_heroku.settings(locals())
